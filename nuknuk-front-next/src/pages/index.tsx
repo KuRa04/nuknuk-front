d8f337743a1de49412f2f492958fd35f6d941c94
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-12-12 16:59:58 +0900

    next-projectの削除

--- nuknuk-front-next/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement }  from 'react'
-import Head from 'next/head'
-import { GetStaticProps } from 'next';
-import StartUp from './start_up';
-
-const Index = (): ReactElement => {
-  return <StartUp />;
-};
-export default Index;
