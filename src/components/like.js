7884ea6358a33008299254fa68182a346d51c0bf
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-12-16 12:02:22 +0900

    ダブルタップでいいね機能実装　componentのlike.jsは使っていないが残しておく

--- src/components/like.js
+++ src/components/like.js
@@ -13,13 +13,13 @@ const Likes = (props) => {
   const postFavorites = async (movie, e) => {
     e.stopPropagation()
     const favorites_db = dbUrl + '/favorites'
-    if (isLiked) {
+    if (props.isLiked) {
       console.log(props.ip_address)
       const params = {movie_id: movie.id, ip_address: props.ip_address}
       console.log(params)
       axios.delete(favorites_db, {data: params}).then((res) => {
         console.log(res.data)
-        const isBool = !isLiked
+        const isBool = !props.isLiked
         setLiked(isBool)
         console.log(isLiked)
         const new_count = res.data.movie_favorites_count
@@ -31,7 +31,7 @@ const Likes = (props) => {
       console.log(dbUrl)
       axios.post(favorites_db, {movie_id: movie.id, ip_address: props.ip_address}).then((res) => {
         console.log(favorites_db)
-        const isBool = !isLiked
+        const isBool = !props.isLiked
         setLiked(isBool)
         const new_count = res.data.movie_favorites_count
         setCount(new_count)
@@ -47,12 +47,12 @@ const Likes = (props) => {
   }, [count])
 
   useEffect(() => {
-    setLiked(isLiked)
-  }, [isLiked])
+    setLiked(props.isLiked)
+  }, [props.isLiked])
 
   return (
     <div className="wrapper_favorites">
-      <img onClick={(e) => postFavorites(props.movie, e)} alt="" width="35" height="35" src={isLiked ? AfterFavoriteImg : BeforeFavoriteImg}  />
+      <img onClick={(e) => postFavorites(props.movie, e)} alt="" width="35" height="35" src={props.isLiked ? AfterFavoriteImg : BeforeFavoriteImg}  />
       <span className="favorites_count">{1}</span>
     </div>
   )
