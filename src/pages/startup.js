32a5071f4f36202c5e3aad682ee7f373afdd59fd
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-12-17 16:16:25 +0900

    使用されていないコードの削除

--- src/pages/startup.js
+++ src/pages/startup.js
@@ -1,18 +1,8 @@
-import React, { useEffect }  from 'react'
-// import { useHistroy } from 'react-router-dom';
+import React from 'react'
 import StartUpImg from '../images/startup.svg'
 import  "../styles/pages/startup.scss";
 
 const StartUp = () => {
-  // const navigate = useHistroy();
-
-  // 3秒後に年齢確認画面に遷移
-  useEffect( () => {
-    window.setTimeout(function(){
-    //  navigate.push("/Movie")
-    }, 1000);
-  });
-
   return (
       <div className="start_up_mapper">
         <img clasName="startup-img" src={StartUpImg} alt=""/>
