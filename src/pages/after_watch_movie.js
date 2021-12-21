6b7388abc45da48df0ef02a6cb5d17d29b182a28
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-12-21 20:40:10 +0900

    使っていないファイルをコメントアウト

--- src/pages/after_watch_movie.js
+++ src/pages/after_watch_movie.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState }  from 'react'
-import "../styles/pages/genre.scss";
-import axios from 'axios';
-import genresController from '../controller/select_genres_controller';
+// import React, { useEffect, useState }  from 'react'
+// import "../styles/pages/genre.scss";
+// import axios from 'axios';
+// import genresController from '../controller/select_genres_controller';
 
 
-const afterWatchMovies = (props) => {
-  const 
-}
+// const afterWatchMovies = (props) => {
+//   const 
+// }
