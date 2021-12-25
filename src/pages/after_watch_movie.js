7dc85bf5c9f880dbe4c3944a8933b1938a8cfb08
Daichi <50697314+KuRa04@users.noreply.github.com>
2021-12-25 17:59:04 +0900

    いらないファイルの削除

--- src/pages/after_watch_movie.js
+++
@@ -1,9 +0,0 @@
-// import React, { useEffect, useState }  from 'react'
-// import "../styles/pages/genre.scss";
-// import axios from 'axios';
-// import genresController from '../controller/select_genres_controller';
-
-
-// const afterWatchMovies = (props) => {
-//   const 
-// }
