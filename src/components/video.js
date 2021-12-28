bdb4ac753e2d5ffb596621c3221aa7f54b9e8b73
murata-naoya <murata.small.baseball@gmail.com>
2021-12-28 13:00:25 +0900

    ミュート属性はずす処理

--- src/components/video.js
+++ src/components/video.js
@@ -4,11 +4,13 @@ import "../styles/components/video.scss"
 const VideoComponent = (props) => {
  return (
   <video
+    muted={true}
     controls={false}
+    playsInline
     width="370"
     height="300"
     poster={props.movie.image}
-    src={props.movie.movie_url +  "#t=3"}
+    src={props.movie.movie_url}
     id={'movie-list-' + props.movie.id}
     preload="metadata"
     ref={props.videoRef}
