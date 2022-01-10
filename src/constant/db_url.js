d4579c0477cb3d9f935319a7e8ac22693b533288
朝倉大智 <daichiasakura@mbp.local>
2022-01-10 12:24:33 +0900

    stg環境のためdb_urlをherokuに変更

--- src/constant/db_url.js
+++ src/constant/db_url.js
@@ -1,3 +1,3 @@
-// export const dbUrl = process.env.REACT_APP_HEROKU_DB_URL;
+export const dbUrl = process.env.REACT_APP_HEROKU_DB_URL;
 // export const dbUrl = process.env.REACT_APP_LOCAL_DB_URL
-export const dbUrl = process.env.REACT_APP_PRODUCTION_DB_URL
+// export const dbUrl = process.env.REACT_APP_PRODUCTION_DB_URL
